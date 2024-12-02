b7e6c31ade7902754dd0780feea5a92723b170ab
GEORGE NERES <68597854+George100Neres@users.noreply.github.com>
2024-12-02 16:10:32 -0300

    Desafio_Final_Vai_Na_Web

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/header/Header'
+import './globalStyle/globalStyle.scss'
+import Footer from './components/footer/Footer.jsx'
+
+
+export default function App() {
+ return (
+  <section>
+    <Header/>
+    <Footer />
+  </section>
+ ) 
+}
